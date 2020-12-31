013421edc0b1a9ccebf1639822286afce60ea155
Mystic Kitty <djabranton@gmail.com>
2020-12-31 00:17:35 -0800

    Created basic homepage.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,40 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_body',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            console.log(dbPostData[0]);
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
