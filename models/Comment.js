ffadab33b80b353bbcd8f0c7772af839a5aae760
Mystic Kitty <djabranton@gmail.com>
2020-12-30 22:15:08 -0800

    Created comments model, associations and routes.

--- models/Comment.js
+++ models/Comment.js
@@ -1,11 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// create our Post model
-class Post extends Model { }
+class Comment extends Model { }
 
-// create fields/columns for Post model
-Post.init(
+Comment.init(
     {
         id: {
             type: DataTypes.INTEGER,
@@ -13,12 +11,8 @@ Post.init(
             primaryKey: true,
             autoIncrement: true
         },
-        title: {
+        comment_text: {
             type: DataTypes.STRING,
-            allowNull: false
-        },
-        post_body: {
-            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 len: [1]
@@ -30,14 +24,21 @@ Post.init(
                 model: 'user',
                 key: 'id'
             }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
         }
     },
     {
         sequelize,
         freezeTableName: true,
         underscored: true,
-        modelName: 'post'
+        modelName: 'comment'
     }
 );
 
-module.exports = Post;
+module.exports = Comment;
