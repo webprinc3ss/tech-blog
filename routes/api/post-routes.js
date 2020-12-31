3a66a97ab0ab33e06e125ae03e1686d8d5444231
Mystic Kitty <djabranton@gmail.com>
2020-12-30 22:42:33 -0800

    Created more routes so comments appear on homepage.

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -8,6 +8,15 @@ router.get('/', (req, res) => {
         attributes: ['id', 'post_body', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
         include: [
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -28,6 +37,15 @@ router.get('/:id', (req, res) => {
         },
         attributes: ['id', 'post_body', 'title', 'created_at'],
         include: [
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
