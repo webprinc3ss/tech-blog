a53a028362dc9f88ce468041e53311f834bf28f9
Mystic Kitty <djabranton@gmail.com>
2021-01-03 20:36:00 -0800

    Changed proxy to secureProxy in server.js.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: process.env.SESSION_SECRET,
     rolling: true, // <-- Set `rolling` to `true`
-    proxy: true,
+    secureProxy: true,
     cookie: {
         httpOnly: true,
         maxAge: 2 * 60 * 60 * 1000 //2 hours;
